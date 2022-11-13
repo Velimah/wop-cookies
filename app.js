e806af0521b827ba4597067a4f9145a87cd4b8b0
veli-matti heino <veli-matti.heino@metropolia.fi>
2022-11-13 17:47:32 +0200

    state management

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 'use strict';
 const express = require('express');
+const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
 
-app.use(cookieParser());
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
+const username = 'foo';
+const password = 'bar';
+
+app.use(cookieParser());
+app.use(session({secret: 'dfgdrdsdfes'}));
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -14,12 +21,35 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
+app.get('/form', (req, res) => {
+  res.render('form');
+});
+
+app.get('/secret', (req, res) => {
+  if(req.session.kirjautunut) {
+    res.render('secret');
+  } else {
+    res.redirect('/form');
+  }
+});
+
+app.post('/login', (req, res) => {
+  const uname = req.body.username;
+  const passwd = req.body.password;
+  if(uname === username && passwd === password) {
+    req.session.kirjautunut = true;
+    res.redirect('/secret');
+  }  else {
+    res.redirect('/form');
+  }
+});
+
 app.get('/setCookie/:clr', (req, res) => {
   res.cookie('color', req.params.clr, {httpOnly: true}).send('cookie set');
 });
 
 app.get('/readCookie', (req, res) => {
-  console.log('Cookies: ', req.cookies);
+  console.log('Cookies: ', req.cookies.color);
   res.send('cookie read');
 });
 
